4563063d684a8ec83cbf505d6086430a5fc1e5b6
Neimar Locatelli <neimar.locatelli@farmaciassaojoao.com.br>
2021-09-14 11:34:05 -0300

    Gerada nova versão
    Gerada nova versão

--- assets/install.sh
+++ assets/install.sh
@@ -8,8 +8,8 @@ mkdir /opt/apps/centralAplicacoes
 cd /opt/apps/centralAplicacoes
 
 # Baixando a app
-rm -rf /opt/apps/centralAplicacoes/Central-aplicacoes-1.0.1.AppImage
-wget -O Central-aplicacoes-1.0.1.AppImage https://nexus.farmaciassaojoao.com.br/repository/static-hosted/central-aplicacoes/Central-aplicacoes-1.0.1.AppImage
+rm -rf /opt/apps/centralAplicacoes/Central-aplicacoes-2.0.0.AppImage
+wget -O Central-aplicacoes-2.0.0.AppImage https://nexus.farmaciassaojoao.com.br/repository/static-hosted/central-aplicacoes/Central-aplicacoes-2.0.0.AppImage
 
 # Baixando a entrada para o menu
 rm -rf /opt/apps/centralAplicacoes/fsj-centralAplicacoes-desktop.desktop
@@ -20,7 +20,7 @@ rm -rf /opt/apps/centralAplicacoes/fsj-centralAplicacoes-desktop.png
 wget https://nexus.farmaciassaojoao.com.br/repository/static-hosted/central-aplicacoes/fsj-centralAplicacoes-desktop.png
 
 # Tornando o app image executavel
-chmod a+x Central-aplicacoes-1.0.1.AppImage
+chmod a+x Central-aplicacoes-2.0.0.AppImage
 
 # Criando a entrada no menu
 cp /opt/apps/centralAplicacoes/fsj-centralAplicacoes-desktop.desktop /usr/share/applications
