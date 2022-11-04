20a9fbb8b0f7ffcbe7cae1cd1adebaaa20ed9f72
Michel Lucio <lucio.michel@gmail.com>
2022-11-04 11:31:34 -0300

    alterações para facilitar o uso

--- main.js
+++ main.js
@@ -1,54 +1,34 @@
+require('update-electron-app')()
 
-const { app, BrowserWindow, ipcMain } = require('electron');
-const { autoUpdater } = require('electron-updater');
+const { app, autoUpdater, dialog } = require('electron')
 
-let mainWindow;
+const server = 'https://your-deployment-url.com'
+const url = `${server}/update/${process.platform}/${app.getVersion()}`
 
-function createWindow () {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
-  mainWindow.loadFile('index.html');
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
-  mainWindow.once('ready-to-show', () => {
-    autoUpdater.checkForUpdatesAndNotify();
-  });
-}
+autoUpdater.setFeedURL({ url })
 
-app.on('ready', () => {
-  createWindow();
-});
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit();
+setInterval(() => {
+  autoUpdater.checkForUpdates()
+}, 60000)
+
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Reiniciar', 'Mais Tarde'],
+    title: 'Atualização Disponivel',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail:
+      'Uma nova versão foi Baixada. Reinicie o Aplicativo para Instalar as Atualizações.',
   }
-});
 
-app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall()
+  })
+})
 
-ipcMain.on('app_version', (event) => {
-    event.sender.send('app_version', { version: app.getVersion() });
-});
-
-autoUpdater.on('update-available', () => {
-    mainWindow.webContents.send('update_available');
-  });
-
-autoUpdater.on('update-downloaded', () => {
-    mainWindow.webContents.send('update_downloaded');
-});
-
-ipcMain.on('restart_app', () => {
-    autoUpdater.quitAndInstall();
-});
+autoUpdater.on('error', (message) => {
+  console.error('Ocorreu um erro ao atualizar o aplicativo')
+  console.error(message)
+})
